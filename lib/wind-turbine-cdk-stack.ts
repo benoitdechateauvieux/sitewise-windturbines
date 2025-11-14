b0b40e3305029c759abf6fed799c30dc5da85b61
Benoit de Chateauvieux <chateauv@amazon.com>
2025-11-14 16:40:39 -0500

    Add models and assets

--- lib/wind-turbine-cdk-stack.ts
+++ lib/wind-turbine-cdk-stack.ts
@@ -1,16 +1,73 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class WindTurbineCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create SiteWise Asset Model for Wind Turbine
+    const windTurbineModel = new cdk.aws_iotsitewise.CfnAssetModel(this, 'WindTurbineModel', {
+      assetModelName: 'WindTurbine',
+      assetModelProperties: [
+        {
+          name: 'Make',
+          dataType: 'STRING',
+          externalId: 'make',
+          type: { 
+            typeName: 'Attribute'
+          }
+        },
+        {
+          name: 'Location',
+          dataType: 'STRING',
+          externalId: 'location',
+          type: { 
+            typeName: 'Attribute'
+          }
+        },
+        {
+          name: 'Torque_KiloNewton_Meter',
+          dataType: 'DOUBLE',
+          externalId: 'torque',
+          type: { 
+            typeName: 'Measurement'
+          }
+        },
+        {
+          name: 'Wind_Direction',
+          dataType: 'DOUBLE',
+          externalId: 'wind_direction',
+          type: { 
+            typeName: 'Measurement'
+          }
+        },
+        {
+          name: 'RotationsPerMinute',
+          dataType: 'DOUBLE',
+          externalId: 'rpm',
+          type: { 
+            typeName: 'Measurement'
+          }
+        },
+        {
+          name: 'Wind_Speed',
+          dataType: 'DOUBLE',
+          externalId: 'wind_speed',
+          type: { 
+            typeName: 'Measurement'
+          }
+        }
+      ]
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'WindTurbineCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create 4 Wind Turbine Assets
+    const turbines = ['Turbine-001', 'Turbine-002', 'Turbine-003', 'Turbine-004'];
+    
+    turbines.forEach((name, index) => {
+      new cdk.aws_iotsitewise.CfnAsset(this, name, {
+        assetName: name,
+        assetModelId: windTurbineModel.attrAssetModelId
+      });
+    });
   }
 }
