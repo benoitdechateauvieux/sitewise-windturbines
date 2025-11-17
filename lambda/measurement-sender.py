6fd8ac36ad9422efaf1d7f92d14785328ad55c6f
Benoit de Chateauvieux <chateauv@amazon.com>
2025-11-17 11:49:06 -0500

    Error handling

--- lambda/measurement-sender.py
+++ lambda/measurement-sender.py
@@ -3,71 +3,79 @@ import json
 import random
 import time
 import os
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def handler(event, context):
-    client = boto3.client('iotsitewise')
-    
-    asset_names = [
-        os.environ['TURBINE_001_NAME'],
-        os.environ['TURBINE_002_NAME'],
-        os.environ['TURBINE_003_NAME'],
-        os.environ['TURBINE_004_NAME']
-    ]
-    
-    
-    for asset_name in asset_names:
-        entries = []
-        timestamp = int(time.time())
+    try:
+        client = boto3.client('iotsitewise')
+        
+        asset_names = [
+            os.environ['TURBINE_001_NAME'],
+            os.environ['TURBINE_002_NAME'],
+            os.environ['TURBINE_003_NAME'],
+            os.environ['TURBINE_004_NAME']
+        ]
+        
+        
+        for asset_name in asset_names:
+            entries = []
+            timestamp = int(time.time())
 
-        entries.append({
-            'entryId': f'{asset_name}-make-{timestamp}',
-            'propertyAlias': f'/{asset_name}/make',
-            'propertyValues': [{
-                'value': {'stringValue': 'Amazon'},
-                'timestamp': {'timeInSeconds': timestamp}
-            }]
-        })
-        entries.append({
-            'entryId': f'{asset_name}-location-{timestamp}',
-            'propertyAlias': f'/{asset_name}/location',
-            'propertyValues': [{
-                'value': {'stringValue': 'Renton'},
-                'timestamp': {'timeInSeconds': timestamp}
-            }]
-        })
-        entries.append({
-            'entryId': f'{asset_name}-torque-{timestamp}',
-            'propertyAlias': f'/{asset_name}/torque',
-            'propertyValues': [{
-                'value': {'doubleValue': random.uniform(100, 500)},
-                'timestamp': {'timeInSeconds': timestamp}
-            }]
-        })
-        entries.append({
-            'entryId': f'{asset_name}-wind_direction-{timestamp}',
-            'propertyAlias': f'/{asset_name}/wind_direction',
-            'propertyValues': [{
-                'value': {'doubleValue': random.uniform(0, 360)},
-                'timestamp': {'timeInSeconds': timestamp}
-            }]
-        })
-        entries.append({
-            'entryId': f'{asset_name}-rpm-{timestamp}',
-            'propertyAlias': f'/{asset_name}/rpm',
-            'propertyValues': [{
-                'value': {'doubleValue': random.uniform(10, 50)},
-                'timestamp': {'timeInSeconds': timestamp}
-            }]
-        })
-        entries.append({
-            'entryId': f'{asset_name}-wind_speed-{timestamp}',
-            'propertyAlias': f'/{asset_name}/wind_speed',
-            'propertyValues': [{
-                'value': {'doubleValue': random.uniform(5, 25)},
-                'timestamp': {'timeInSeconds': timestamp}
-            }]
-        })
-            
-        response = client.batch_put_asset_property_value(entries=entries)
-        print(f"Successfully sent {len(entries)} measurements")
-        print(f"Response: {response}")
+            entries.append({
+                'entryId': f'{asset_name}-make-{timestamp}',
+                'propertyAlias': f'/{asset_name}/make',
+                'propertyValues': [{
+                    'value': {'stringValue': 'Amazon'},
+                    'timestamp': {'timeInSeconds': timestamp}
+                }]
+            })
+            entries.append({
+                'entryId': f'{asset_name}-location-{timestamp}',
+                'propertyAlias': f'/{asset_name}/location',
+                'propertyValues': [{
+                    'value': {'stringValue': 'Renton'},
+                    'timestamp': {'timeInSeconds': timestamp}
+                }]
+            })
+            entries.append({
+                'entryId': f'{asset_name}-torque-{timestamp}',
+                'propertyAlias': f'/{asset_name}/torque',
+                'propertyValues': [{
+                    'value': {'doubleValue': random.uniform(100, 500)},
+                    'timestamp': {'timeInSeconds': timestamp}
+                }]
+            })
+            entries.append({
+                'entryId': f'{asset_name}-wind_direction-{timestamp}',
+                'propertyAlias': f'/{asset_name}/wind_direction',
+                'propertyValues': [{
+                    'value': {'doubleValue': random.uniform(0, 360)},
+                    'timestamp': {'timeInSeconds': timestamp}
+                }]
+            })
+            entries.append({
+                'entryId': f'{asset_name}-rpm-{timestamp}',
+                'propertyAlias': f'/{asset_name}/rpm',
+                'propertyValues': [{
+                    'value': {'doubleValue': random.uniform(10, 50)},
+                    'timestamp': {'timeInSeconds': timestamp}
+                }]
+            })
+            entries.append({
+                'entryId': f'{asset_name}-wind_speed-{timestamp}',
+                'propertyAlias': f'/{asset_name}/wind_speed',
+                'propertyValues': [{
+                    'value': {'doubleValue': random.uniform(5, 25)},
+                    'timestamp': {'timeInSeconds': timestamp}
+                }]
+            })
+                
+            response = client.batch_put_asset_property_value(entries=entries)
+            logger.info(f"Successfully sent {len(entries)} measurements for {asset_name}")
+    
+    except Exception as e:
+        logger.error(f"Error sending measurements: {str(e)}")
+        raise
--- lambda/query-executor.py
+++ lambda/query-executor.py
@@ -1,64 +1,74 @@
 import boto3
 import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def handler(event, context):
-    sitewise = boto3.client('iotsitewise')
-    
-    make = event.get('make', 'Amazon')
-    location = event.get('location', 'Renton')
-    rpm_threshold = event.get('rpm_threshold', 25)
-    torque_threshold = event.get('torque_threshold', 300)
-    wind_speed_threshold = event.get('wind_speed_threshold', 15)
-    wind_direction_threshold = event.get('wind_direction_threshold', 100)
-    
-    query = f'''
-    SELECT
-      asset_id,
-      asset_name
-    FROM
-      asset
-    WHERE asset_id IN (
-        SELECT asset_id
-        FROM latest_value_time_series
-        WHERE SUBSTR(property_alias, '[^/]+$') = 'location'
-        AND string_value = '{location}'
-    )
-    AND asset_id IN (
-        SELECT asset_id
-        FROM latest_value_time_series
-        WHERE SUBSTR(property_alias, '[^/]+$') = 'make'
-        AND string_value = '{make}'
-    )
-    AND (
-        asset_id IN (
+    try:
+        sitewise = boto3.client('iotsitewise')
+        
+        make = event.get('make', 'Amazon')
+        location = event.get('location', 'Renton')
+        rpm_threshold = event.get('rpm_threshold', 25)
+        torque_threshold = event.get('torque_threshold', 300)
+        wind_speed_threshold = event.get('wind_speed_threshold', 15)
+        wind_direction_threshold = event.get('wind_direction_threshold', 100)
+        
+        query = f'''
+        SELECT
+          asset_id,
+          asset_name
+        FROM
+          asset
+        WHERE asset_id IN (
             SELECT asset_id
             FROM latest_value_time_series
-            WHERE SUBSTR(property_alias, '[^/]+$') = 'rpm'
-            AND double_value > {rpm_threshold}
+            WHERE SUBSTR(property_alias, '[^/]+$') = 'location'
+            AND string_value = '{location}'
         )
-        OR asset_id IN (
+        AND asset_id IN (
             SELECT asset_id
             FROM latest_value_time_series
-            WHERE SUBSTR(property_alias, '[^/]+$') = 'torque'
-            AND double_value > {torque_threshold}
+            WHERE SUBSTR(property_alias, '[^/]+$') = 'make'
+            AND string_value = '{make}'
         )
-        OR (
+        AND (
             asset_id IN (
                 SELECT asset_id
                 FROM latest_value_time_series
-                WHERE SUBSTR(property_alias, '[^/]+$') = 'wind_speed'
-                AND double_value > {wind_speed_threshold}
+                WHERE SUBSTR(property_alias, '[^/]+$') = 'rpm'
+                AND double_value > {rpm_threshold}
             )
-            AND asset_id IN (
+            OR asset_id IN (
                 SELECT asset_id
                 FROM latest_value_time_series
-                WHERE SUBSTR(property_alias, '[^/]+$') = 'wind_direction'
-                AND double_value > {wind_direction_threshold}
+                WHERE SUBSTR(property_alias, '[^/]+$') = 'torque'
+                AND double_value > {torque_threshold}
+            )
+            OR (
+                asset_id IN (
+                    SELECT asset_id
+                    FROM latest_value_time_series
+                    WHERE SUBSTR(property_alias, '[^/]+$') = 'wind_speed'
+                    AND double_value > {wind_speed_threshold}
+                )
+                AND asset_id IN (
+                    SELECT asset_id
+                    FROM latest_value_time_series
+                    WHERE SUBSTR(property_alias, '[^/]+$') = 'wind_direction'
+                    AND double_value > {wind_direction_threshold}
+                )
             )
         )
-    )
-    ORDER BY asset_name
-    '''
+        ORDER BY asset_name
+        '''
+        
+        logger.info(f"Executing query with parameters: make={make}, location={location}")
+        response = sitewise.execute_query(queryStatement=query)
+        return {'statusCode': 200, 'body': json.dumps(response)}
     
-    response = sitewise.execute_query(queryStatement=query)
-    return {'statusCode': 200, 'body': json.dumps(response)}
+    except Exception as e:
+        logger.error(f"Error executing query: {str(e)}")
+        return {'statusCode': 500, 'body': json.dumps({'error': str(e)})}
